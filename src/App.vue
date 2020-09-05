28328adedbf44898a58d5210d01ec51af68f999e
phweb <phweb53@gmail.com>
2020-09-05 10:47:01 -0300

    Iniciando projeto

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <PokemonList/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import PokemonList from '@/components/PokemonList.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    PokemonList,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <PokemonList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import PokemonList from '@/components/PokemonList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    PokemonList
+  }
+}
+</script>
