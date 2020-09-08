6f9d23013b8f88301fc7fcbf3919f6c4506ea555
phweb <phweb53@gmail.com>
2020-09-08 08:03:35 -0300

    Adicionado PWA no projeto

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+
 module.exports = {
+ 
+  pwa: {
+    name: 'Pokemon',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ]
